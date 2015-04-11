ae279b0e1b30053cfc9beed013e9589d137ad79c
hiratsukashu <hiratsukashu@indival.co.jp>
2015-04-11 11:23:44 +0900

    Initial Commit

---
+++ ConnectionBySession/ConnectionBySession.h
@@ -0,0 +1,19 @@
+//
+//  ConnectionBySession.h
+//  ConnectionBySession
+//
+//  Created by 平塚 俊輔 on 2015/04/11.
+//  Copyright (c) 2015年 平塚 俊輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnectionBySession.
+FOUNDATION_EXPORT double ConnectionBySessionVersionNumber;
+
+//! Project version string for ConnectionBySession.
+FOUNDATION_EXPORT const unsigned char ConnectionBySessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectionBySession/PublicHeader.h>
+
+
