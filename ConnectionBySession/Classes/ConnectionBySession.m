948e9b0fd09aa4b57b40cf555a50c04b9de4cbe4
hiratsukashu <hiratsukashu@indival.co.jp>
2015-04-12 11:50:31 +0900

    first commit

---
+++ ConnectionBySession/Classes/ConnectionBySession.m
@@ -0,0 +1,18 @@
+//
+//  ConnectionBySession.m
+//  ConnectionBySession
+//
+//  Created by 平塚 俊輔 on 2015/04/09.
+//  Copyright (c) 2015年 平塚 俊輔. All rights reserved.
+//
+
+#import "ConnectionBySession.h"
+
+
+
+@implementation ConnectionBySession
+
+
+
+
+@end
